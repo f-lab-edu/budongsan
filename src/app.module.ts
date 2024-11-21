c71e4173f337d7eccf8f0a32f847075bd7660e6e
sejin5 <sejin5.dev@gmail.com>
2024-11-21 12:24:19 +0900

    [#3] 회원가입 API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+   imports: [
+   ConfigModule.forRoot({
+    envFilePath: [
+      (process.env.NODE_ENV === 'production') ? '.production.env'
+       : (process.env.NODE_ENV === 'stage') ? '.stage.env' : '.development.env'
+    ],
+    isGlobal: true,
+  }),
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'conn_test',
+      entities: [__dirname + `/**/*.entity{.ts,.js}`],
+      synchronize: false
+    }),
+    AuthModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ],
 })
 export class AppModule {}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,41 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, ConflictException } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { AuthDTO } from 'src/auth/dto/auth.dto';
+import { userDto } from './dto/user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  //TODO
+  //가입, 로그인, 탈퇴, 수정, 아이디 패스워드 찾기, 이메일 인증?
+
+  @Post('/signup')
+  async signup (@Body() authDto: AuthDTO.SignUp) {
+    const {userId, userName, email, password} = authDto;
+    
+    const userEntity = this.usersService.createUser(authDto);
+
+    return '회원가입성공';
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
