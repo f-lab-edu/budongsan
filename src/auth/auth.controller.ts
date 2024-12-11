912164f3609185c1dead9c97fbca75c62395c0a5
sejin5 <sejin5.dev@gmail.com>
2024-12-11 18:19:07 +0900

    [#7] JWT인증구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './security/passport.jwt';
 
-@Controller('auth')
+@Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/signin')
+  @UseGuards(LocalAuthGuard)
+  async singin(@Request() req){
+    return this.authService.signin(req.user);
+  }
 }
