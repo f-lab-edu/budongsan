9ab3e610c775fee1396eade3d08e49f3c4b6a527
sejin5 <sejin5.dev@gmail.com>
2025-01-15 16:19:12 +0900

    [#16] 게시판 CRUD 구현

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { BoardsEntity } from './entities/boards.entity';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { UpdateBoardDto } from './dto/update-board.dto';
 
 @Injectable()
 export class BoardsService {
-    create(createBoardDto: CreateBoardDto) {
-        return 'pass?';
+    constructor(
+        @InjectRepository(BoardsEntity)
+        private boardsRepository: Repository<BoardsEntity>
+    ) { }
+
+    async create(createBoardDto: CreateBoardDto) {
+        console.log('hello again', createBoardDto);
+        const boardEntity = await this.boardsRepository.create(createBoardDto);
+
+        return await this.boardsRepository.save(boardEntity);
+    }
+
+    async readOne(boardId: number) {
+        return await this.boardsRepository.findOne({
+            where: {
+                boardId
+            }
+        })
+    }
+
+    readAll() {
+        return this.boardsRepository.find();
+    }
+
+    edit(updateBoardDto: UpdateBoardDto) {
+        const boardId = updateBoardDto.boardId;
+        return this.boardsRepository.update(boardId, updateBoardDto);
+    }
+
+    remove(boardId: number) {
+        return this.boardsRepository.softDelete(boardId);
     }
 }
--- src/boards/entities/boards.entity.spec.ts
+++ src/boards/entities/boards.entity.spec.ts
@@ -3,14 +3,16 @@ import { BoardsEntity as Boards } from './boards.entity';
 describe('title validate', () => {
     it('should return back a Error', () => {
         const title = '';
-        const spaceShipId = () => Boards.validate(title);
+        const contents = '';
+        const spaceShipId = () => Boards.validate(title, contents);
 
         expect(spaceShipId).toThrow(Error);
     });
 
     it('should return back a valid title', () => {
         const title = 'g';
+        const contents = 'script';
 
-        expect(Boards.validate(title)).toBeTruthy();
+        expect(Boards.validate(title, contents)).toBeTruthy();
     });
 });
